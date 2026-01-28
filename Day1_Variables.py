240d4d46795df4fbaa767473aab5a8bb9d9164ea
Noxagon <nokkhotrorahman639@gmail.com>
2026-01-28 23:51:35 +0600

    Add Day1_Variables.py with variable examples

---
+++ Day1_Variables.py
@@ -0,0 +1,55 @@
+#Day 2: 30 Days of python programming
+import math
+
+first_name="Nokkhotro"
+last_name="Rahman"
+full_name=first_name+" "+last_name
+country="Bangladesh"
+city="Dhaka"
+age=21
+year=2026
+is_married,is_true,is_light_on=False,True,True
+interests=['gaming', 'cycling', 'cars', 'philosophy', 'psychology', 'politics', 'history']
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(interests))
+print(len(first_name))
+print(len(last_name))
+print(len(first_name)==len(last_name))
+
+num_one=5
+num_two=4
+add=num_one+num_two
+sub=num_one+num_two
+mul=num_one*one+num_two
+div=num_one/one+num_two
+remainder=num_two%num_one
+exp=num_one**num_two
+floordiv=num_one//num_two
+print(add,sub,mul,div,remainder,exp,floordiv)
+print(full_name)
+
+r=30
+area_of_circle=math.pi*(r**2)
+circum_of_circle=2*math.pi*r
+print(area_of_circle,circum_of_circle)
+
+r2=float(input())
+area_of_circle2=math.pi*(r2**2)
+print(area_of_circle2)
+
+first_name2=input()
+last_name2=input()
+full_name2=first_name2+" "+last_name2
+country2=input()
+age2=int(input())
+print(full_name2,country2,age2)
+help('keywords')
